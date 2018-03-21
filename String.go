6d88c83545fdc2e86da7819410d894dbadfaf058
Neo J Rotary <a081483@gmail.com>
2018-03-21 20:46:05 +0900

    update string

--- String.go
+++ String.go
@@ -13,6 +13,17 @@ func String(obj string) *TypeString {
 	return &TypeString{Obj: obj, Index: -1}
 }
 
+// Copy get copy of type
+func (s *TypeString) Copy() *TypeString {
+	return &TypeString{Obj: s.Obj, Index: s.Index}
+}
+
+// Update update object
+func (s *TypeString) Update(str string) *TypeString {
+	s.Obj = str
+	return s
+}
+
 // Get return object string
 func (s *TypeString) Get() string {
 	return s.Obj
@@ -30,12 +41,12 @@ func (s *TypeString) HasSuffix(suffix string) bool {
 
 // Replace wrapper of strings.Replace()
 func (s *TypeString) Replace(old string, new string, n int) *TypeString {
-	return String(strings.Replace(s.Obj, old, new, n))
+	return s.Update(strings.Replace(s.Obj, old, new, n))
 }
 
 // ReplaceAll wrapper of strings.Replace(), set n = -1
 func (s *TypeString) ReplaceAll(old string, new string) *TypeString {
-	return String(strings.Replace(s.Obj, old, new, -1))
+	return s.Update(strings.Replace(s.Obj, old, new, -1))
 }
 
 // ReplaceTo replace element to target slice by index. Panic if element doesn't have index.
@@ -59,10 +70,10 @@ func (s *TypeString) Split(sep string) *TypeStringSlice {
 
 // Trim wrapper of strings.Trim()
 func (s *TypeString) Trim(cutset string) *TypeString {
-	return String(strings.Trim(s.Obj, cutset))
+	return s.Update(strings.Trim(s.Obj, cutset))
 }
 
 // TrimSpace wrapper of strings.TrimSpace()
 func (s *TypeString) TrimSpace() *TypeString {
-	return String(strings.TrimSpace(s.Obj))
+	return s.Update(strings.TrimSpace(s.Obj))
 }
