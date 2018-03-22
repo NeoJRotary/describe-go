f2480592b91ea1aaf4f7b8a895a5cea1370c5d94
Neo J Rotary <a081483@gmail.com>
2018-03-22 12:02:38 +0900

    update stringslice

--- StringSlice.go
+++ StringSlice.go
@@ -97,6 +97,11 @@ func (ss *TypeStringSlice) Has(elm string) bool {
 	return false
 }
 
+// NotHas whether it has element
+func (ss *TypeStringSlice) NotHas(elm string) bool {
+	return !ss.Has(elm)
+}
+
 // Trim remove target element from both sides of slice
 func (ss *TypeStringSlice) Trim(elm string) *TypeStringSlice {
 	var start, end int
